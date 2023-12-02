e517d0c46b01d127410b1d78181a771828cfc2a1
Blaze276_ <109112309+Blaze276@users.noreply.github.com>
2023-12-01 19:48:13 -0500

    Update bot.lua

--- The Game Bot/bot.lua
+++ The Game Bot/bot.lua
@@ -25,6 +25,7 @@ local Quotas = {
     "The Among us OST",
     "The Fall Guys OST",
     "The Geometry Dash OST",
+    "The Fortnite OST",
 }
 
 client:on('ready', function()
@@ -60,7 +61,7 @@ function CreateEmbed(fields, title, description)
         },
         footer = {
             text = 'The Game Bot',
-            icon_url = 'https://cdn.discordapp.com/avatars/1145327542723686451/aa8e209b03194832e7b3704eef3fd297.png?size=4096'
+            icon_url = 'https://cdn.discordapp.com/avatars/1145327542723686451/b7d10e6d9a87ab39e1e2f2ca6a8bc663.png?size=4096'
         },
         url = nil,
         author = {
@@ -68,6 +69,9 @@ function CreateEmbed(fields, title, description)
             url = nil,
             icon_url = nil
         },
+        thumbnail = {
+            url = "",
+        }
     }
     return embed
 end
@@ -169,21 +173,25 @@ function processCMD(msg,channel,message)
             end
             if ActualMessage == "mute" then
                 local mentionedUserID = string.sub(args[1],3,#args[1]-1)
+                local user = message.guild:getMember(string.sub(args[1],3,#args[1]-1))
+                local username = user.username
                 if guild then
                     local member = guild:getMember(mentionedUserID)
                     local mutedRole = guild.roles:find(function(role)
                         return role.name == 'Muted'
                     end)
                     if member.id == "960887298533244928" then
-                        sendMessage("Error muting "..args[1]..": no permissions",channel)
+                        sendMessage("Error muting ".."**"..username.."**".." : no permissions",channel)
                         return
                     end
                     member:addRole(mutedRole)
-                    sendMessage("Muted "..args[1],channel)
+                    sendMessage("Muted ".."**"..username.."**",channel)
                 end
             end
             if ActualMessage == "unmute" then
                 local mentionedUserID = string.sub(args[1],3,#args[1]-1)
+                local targetMember = message.guild:getMember(string.sub(args[1],3,#args[1]-1))
+                local username = targetMember.username
                 if guild then
                     local member = guild:getMember(mentionedUserID)
                     local mutedRole = guild.roles:find(function(role)
@@ -191,7 +199,7 @@ function processCMD(msg,channel,message)
                     end)
                     member:removeRole(mutedRole)
                     -- member:addPermission(discordia.enums.permission.sendMessages)
-                    sendMessage("Unmuted "..args[1],channel)
+                    sendMessage("Unmuted ".."**"..username.."**",channel)
                 end
             end
             if ActualMessage == "ban" then
@@ -261,13 +269,13 @@ function processCMD(msg,channel,message)
                 sendMessage("rizzard",channel)
                 sendMessage("just like my lizard",channel)
             end
-            if ActualMessage == "getonline" then
-                local member = guild:getMember(string.sub(args[1],3,#args[1]-1))
-                local status = member.status
-                print(status,"Balls",member.username)
+            -- if ActualMessage == "getonline" then
+            --  local member = guild:getMember(string.sub(args[1],3,#args[1]-1))
+            --  local status = member.status
+            --  print(status,"Balls",member.username)
                 -- Check the member's status and reply accordingly
-                message:reply("The user is currently "..status..".")
-            end
+            --  message:reply("The user is currently "..status..".")
+            --end
             if ActualMessage == "modapply" then
                 local emb = CreateEmbed({},"Mod Form","Thanks for applying. Please allow at most 48hrs for your mod application to be looked at.")
                 emb.url = "https://forms.gle/euSvwrb6JyoW8cPr8"
@@ -366,8 +374,8 @@ function processCMD(msg,channel,message)
             end
             if ActualMessage == "forks" then
                 local bawls = CreateEmbed({}, "The Game Bot Forks", "Forks of the game bot to different languages")
-                bawls.field[#bawls.field] = {name="The Game Bot Python", value="The Game bot coded in python and the first gamebot to not be coded in blocks", inline=false}
-                bawls.field[#bawls.field] = {name="The Game Bot Lua", value="The Game bot coded in lua. second gamebot to be made and not complete just yet. main option for gamebot once it is completed", inline=false}
+                bawls.fields[#bawls.fields+1] = {name="The Game Bot Python", value="The Game bot coded in python and the first gamebot to not be coded in blocks", inline=false}
+                bawls.fields[#bawls.fields+1] = {name="The Game Bot Lua", value="The Game bot coded in lua. second gamebot to be made and not complete just yet. main option for gamebot once it is completed", inline=false}
                 channel: send{embed=bawls}
 
             end
@@ -399,14 +407,16 @@ function processCMD(msg,channel,message)
             if ActualMessage == "sponsored" then
                 sendMessage("**You Know The Type**",channel)
                 sendMessage("https://cdn.discordapp.com/attachments/1119586233161621525/1167639789693042788/IMG_6849.jpg?ex=654edc4b&is=653c674b&hm=7bbe4374d3ad94cb216f617c15621e8ecfd4d5d3b3be622977934f242215ac56&", channel)
-                sendMessage("[_Check out Brandist!_](https://www.youknowthetype.com)")
+                sendMessage("[_Check out Brandist!_](https://www.youknowthetype.com)", channel)
             end
             if ActualMessage =="freedom" then
-                sendMessage("https://youtu.be/mQ5khClAFIo?si=LYPdSJL-vomEISf3")
+                sendMessage("https://youtu.be/mQ5khClAFIo?si=LYPdSJL-vomEISf3", channel)
 
             end
             if ActualMessage == "warn" then
                 local author = message.guild:getMember(message.author.id)
+                local targetMember = message.guild:getMember(string.sub(args[1],3,#args[1]-1))
+                local username = targetMember.username
                 if not author:hasPermission("kickMembers") then
                     sendMessage("You do not have the required perms to use this command.", channel)
                     return
@@ -420,7 +430,7 @@ function processCMD(msg,channel,message)
                 fullReason = string.sub(fullReason,1,#fullReason-1)
 
                 local Warns = io.open("warns.txt","a")
-                sendMessage("Warned user "..args[1].." for "..fullReason,channel)
+                sendMessage("Warned user ".."**"..username.."**".." for "..fullReason,channel)
                 Warns:write(string.sub(args[1],3,#args[1]-1)..":"..fullReason)
                 Warns:write("\n")
                 Warns:close()
@@ -488,6 +498,63 @@ function processCMD(msg,channel,message)
             if ActualMessage == "invite" then
                 sendMessage("You can invite The Game Bot with this link:",channel)
                 sendMessage("**[Invite The Game Bot](https://discord.com/api/oauth2/authorize?client_id=1145327542723686451&permissions=8&scope=bot%20applications.commands)**",channel)
+            end
+            if ActualMessage == "fandom" then
+                local pages = {
+                    ["blaze"] = {
+                        pfp = "https://cdn.discordapp.com/avatars/960887298533244928/62ab0cb4062aa3dee62f77618eb0c6a7.png?size=4096",
+                        title = "Blaze",
+                        desc = "Blaze276, also known as Taavi Nehemia is the creator and owner of GTTP. he is described as \"subonpatreon\", \"Bald\", and \"Handsome\", Blaze has also cheated in a Minecraft survival by mining resources with baritone, he is from australia and has lived there for all his life up until the tail end of 2019, he then left Unison to go to Australia for 2 months as a vacation and has stated that \"Im going to Australia for 2 months and when i get back to america ill probably move to a different state.\". He currently resides in Perth, WA, (formerly Brooklyn, NY). He is decently skilled at coding with lua and has made this bot right here. He is good at Geometry dash and has beat Stereo Madness, Back on track, Polargeist, Dry out, Base after base, Can't let go, Jumper, Time Machine, Cycles, and Theory of everything. He is very good at minecraft (except for building).",
+                        quote = "\"Continuity is pretty bloodthirsty, i will admit.\" -Blaze276 talking to almond about Continuity (netherite axe)",
+                        age = "13",
+                        school = "Urban Assembly Unison School, (formerly)",
+                    },
+                    ["racspeedster"] = {
+                        pfp = "https://images-ext-1.discordapp.net/external/pg0SB4GaovN9ToIMMhBGq5rRjMXaYrimBgqMmIMFW_Y/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/973292238618636369/59bd864a40948444219206c9634db92b.png?width=468&height=468",
+                        title = "RACSpeedster",
+                        desc = "RACSpeedster, also known as Kaleb Prince, is a notable member of GTTP and a co-Developer to The Game Bot and the sole developer of the arsenal remake. he is described as \"Funny\",\"Good\", and \"a coding genius -Blaze276\". He describes himself as the Literal definition of a computer nerd. Got a programming problem? Ask him.",
+                        quote = "\"OH NOES!! Line 1730 of A2Client.lua is giving me an error! Oh.. I missed a single character. BLAZE! LOOK AT THIS!!! LOOK AT MY MISTAKE!!!!\" -RACSpeedster telling Blaze about an error in his code",
+                        age = "13",
+                        school = "Urban Assembly Unison School",
+                    },
+                    ["tbd_entity_bl8ze_badguys_movie_white_cracker_loser_bad_at_life_and_minecraft_baby_rippel_guy_male_large_older_13"] = {
+                        pfp = "https://cdn.discordapp.com/attachments/1118509756492021802/1162735257074794536/7583a56c23b85a3f32d91e89e17c5838.png?ex=653d0497&is=652a8f97&hm=5702135695dd02d07f185386782af9251c96fe5bb5f2e167fca9053afee0e6ce&",
+                        title = "Bl8ze",
+                        desc = "Bl8ze is a notable member of gttp. He was an original member of the five musketeers (suceeded by dork7), and played in the original Gttp (now ALT) Survival server. He is one of the few members to get their own custom role in the discord server (later removed after testing Blaze276). Bl8ze left Gttp in july of 2023 and rejoined in september on the grounds of \"I just want to use the voice channels and play the minecraft server\", since that statement he has done much more than that, he then left again on the 13 of october and preceded to get his minecraft server playerdata deleted and his discord and minecraft accounts banned from the respective servers, Due to bullying users and verbally abusing members. He is \"Spiteful\", \"Jealous\", and \"a psychologically messed up person\". He and his shenanegans have caused a new rule to be added to gttp stating \"no messages or forwarded messages from a user called Bl8ze360, will result in warn\"- rule no. 9. Due to him being an [insert-topic] topic we dont talk about him.",
+                        quote = "\"yeah..., cuz im just so cool.\" -Bl8ze talking to Blaze about him \"being cool\".",
+                        age = "13",
+                        school = "Urban Assembly Unison School",
+                    },
+                    ["orangutanbanana"] = {
+                        pfp = "https://cdn.discordapp.com/avatars/700515277904805989/630859fe042250736e9fe8c5f4541c29.png?size=4096",
+                        title = "Orangutang Banana",
+                        desc = "Orangutang Banana, also known as Jaxx Marquez is a notable member of GTTP. He is described as \"uplifting\",\"Playful\",\"legend -dork7\". He is the first ever moderator on GTTP history, (succeded by Star2080) and the first ever administrator in GTTP history, and the fifth member of the current Musketeers. He helped to create the future GTTP roadmap.",
+                        quote = "\"quote\" -quote by Orangutang Banana",
+                        age = "13",
+                        school = "removed at request",
+                    },
+                    ["almond"] = {
+                        pfp = "https://cdn.discordapp.com/avatars/1044740815743897730/7f4cea419723b26d0de0f68d268fea72.png?size=4096",
+                        title = "Almond",
+                        desc = "almond, also known as [removed] is a member of GTTP. He is described as \"adventurous\",\"annoying\",\"a btd6 pro -dork7\". He is siblings with Bl8ze and is the only Rippel left in GTTP. He was part of the original origins server and is an original musketeer. He had a tantrum about Bl8zes page to the fandom page and threatened to report Blaze276 for having a page about Bl8ze. He is the most banned user in GTTP history having being banned over 5 times!",
+                        quote = "\"Loser alert... fr tho\" -almond telling live_laugh_leave_me_alone that he was a loser",
+                        age = "11",
+                        school = "Urban Assembly Unison School",
+                    }
+                }
+                local page = pages[string.lower(args[1])]
+                local embed = CreateEmbed({},page.title,page.desc)
+                embed.thumbnail.url = page.pfp
+                embed.fields[#embed.fields+1] = {name = "Quote:", value = page.quote, inline = false}
+                embed.fields[#embed.fields+1] = {name = "Age:", value = page.age, inline = false}
+                embed.fields[#embed.fields+1] = {name = "School:", value = page.school, inline = false}
+                channel:send{embed = embed}
+            end
+            if ActualMessage == "fire" then
+                sendMessage("https://youtu.be/vj8ZW0jr83I?si=oCZKkpKXX3VRGzmi", channel)
+            end
+            if ActualMessage == "drake" then
+                sendMessage("https://cdn.discordapp.com/attachments/1124807018662867065/1180307593898491965/IMG_7799.jpg?ex=657cf21b&is=656a7d1b&hm=3206af476b6e00a33f4ac2c81870831f2c0784cd541a350bfe4c4176e41cd870&", channel)
             end
         end
     end
