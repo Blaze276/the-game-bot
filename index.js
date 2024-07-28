63f6e97fb8eea7037805b7e8d289b6366cb9b2e9
Blaze276_ <109112309+Blaze276@users.noreply.github.com>
2024-07-28 15:03:38 +0930

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const fs = require("fs");
 const client = new Discord.Client({
   intents: []
 });
+
+// keepalive
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('a');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 // slash commands shit
 client.commands = new Collection(); // Initialize client.commands
 
