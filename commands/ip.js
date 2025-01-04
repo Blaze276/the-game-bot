c5cb7767df9c6d7611bada3910963d98090f9287
Blaze276_ <109112309+Blaze276@users.noreply.github.com>
2025-01-04 18:07:35 +1030

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -4,21 +4,21 @@ const ipCommand = {
     data: new SlashCommandBuilder()
         .setName('ip')
         .setDescription('Get server IP address')
-        .addStringOption(option =>
+        /*.addStringOption(option =>
             option
                 .setName('bedrock')
-                .setDescription('Gets the bedrock server IP address (add anything to the option!)')
+                .setDescription('Gets the bedrock server IP address (add any value to the option!)')
                 .setRequired(false)
-        ),
+        )*/,
     async execute(interaction) {
-        const bedrock = interaction.options.getString('bedrock');
+        //const bedrock = interaction.options.getString('bedrock');
 
-        if (bedrock) {
-            await interaction.reply('The Bedrock IP is **soxthesigma.my.pebble.host** \nport **8126**');
-        } else {
-            await interaction.reply('The Java IP is **soxthesigma.my.pebble.host** \nversion **1.16.5**');
+       // if (bedrock) {
+           // await interaction.reply('The Bedrock IP is **soxthesigma.my.pebble.host** \nport **8126**\n You can also find another method to login to the server at this (website)[https://Le-Server-docs.onrender.com]');
+       // } else {
+            await interaction.reply('The IP is **soxthesigma.my.pebble.host** \nversion **1.18.1**');
         }
-    }
+    
 };
 
 module.exports = ipCommand;
