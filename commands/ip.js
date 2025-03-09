044cc675ef0f5da6c15590c83ff105492ec3c1db
Blaze276_ <109112309+Blaze276@users.noreply.github.com>
2025-03-10 01:53:40 +1030

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -4,19 +4,19 @@ const ipCommand = {
     data: new SlashCommandBuilder()
         .setName('ip')
         .setDescription('Get server IP address')
-        /*.addStringOption(option =>
+        .addStringOption(option =>
             option
                 .setName('bedrock')
                 .setDescription('Gets the bedrock server IP address (add any value to the option!)')
                 .setRequired(false)
-        )*/,
+        ),
     async execute(interaction) {
-        //const bedrock = interaction.options.getString('bedrock');
+        const bedrock = interaction.options.getString('bedrock');
 
-       // if (bedrock) {
-           // await interaction.reply('The Bedrock IP is **soxthesigma.my.pebble.host** \nport **8126**\n You can also find another method to login to the server at this (website)[https://Le-Server-docs.onrender.com]');
-       // } else {
-            await interaction.reply('The IP is **soxthesigma.my.pebble.host** \nversion **1.20.1**\n**REQUIRES MODPACK**');
+       if (bedrock) {
+            await interaction.reply('The Bedrock IP is **soxthesigma.my.pebble.host** \nport **8135**\n there is also the \'le server method\'');
+       } else {
+            await interaction.reply('The IP is **soxthesigma.my.pebble.host** \nversion **1.16.5** or above');
         }
     
 };
