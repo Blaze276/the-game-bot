1c5b606c568c5adcd2b47e4bced0a31e49da3094
Blaze276_ <109112309+Blaze276@users.noreply.github.com>
2025-04-05 21:11:53 +1030

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -13,10 +13,10 @@ const ipCommand = {
     async execute(interaction) {
         const bedrock = interaction.options.getString('bedrock');
 
-       if (bedrock) {
-            await interaction.reply('The Bedrock IP is **soxthesigma.my.pebble.host** \nport **8135**\n there is also the \'le server method\'');
-       } else {
-            await interaction.reply('The IP is **soxthesigma.my.pebble.host** \nversion **1.16.5** or above');
+       if (bedrock) { // The Bedrock IP is **soxthesigma.my.pebble.host** \nport **8135**\n there is also the \'le server method\'
+            await interaction.reply('**This subcommand is disabled**');
+       } else { // The IP is **soxthesigma.my.pebble.host** \nversion **1.16.5** or above
+            await interaction.reply('**The command _\'/ip\'_ is disabled');
         }
     }
 };
